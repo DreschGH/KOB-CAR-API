da6f75dcf896b76ec81a4cd006b3c336112e3364
DreschGH <gabriel.dresch.sc@gmail.com>
2025-11-26 16:09:20 -0300

    Remove arquivos específicos do rastreamento

--- gcsHandler.py
+++
@@ -1,5 +0,0 @@
-'''
-
-Avaliando implementação do leitor do bucket no GCS para consumo de dados no qlik
-
-'''
--- rotina_atualizardbs.py
+++
@@ -1,4 +0,0 @@
-from kob_car_api.common import CommonOps
-
-operator = CommonOps(db_path='data/kob_car.db')
-operator.sync_ops_tables(n=2)
